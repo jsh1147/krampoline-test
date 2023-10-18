96eba206440cbf419a57d2c91ccfa2cb975592d8
JsH <jsh1147@naver.com>
2023-10-18 19:11:32 +0900

    feat: krampoline 변경사항 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const env = loadEnv(process.cwd(), '')
-const staticServerUri = env.VITE_APP_PATH || "";
-
 export default defineConfig({
-  base: staticServerUri,
   server: {
     port: 3000,
   },
